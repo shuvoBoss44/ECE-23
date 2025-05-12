f6582491ebe3647570b9935abda83012be963d4c
shuvoBoss44 <chakmashuvo2016@gmail.com>
2025-05-12 18:40:45 +0600

    adding search bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "/Users/shuvochakma/React/Learning/src/index.css";
+import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
